f6b33734d68d2e318cdc0a07b4cfac765d6d5c87
rishadroshanpt <rishadroshan27778@gmail.com>
2025-01-10 16:05:49 +0530

    student management system using api and js done

---
+++ studentManagement/app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class student(models.Model):
+    rollNo=models.IntegerField()
+    name=models.TextField()
+    course=models.TextField()
+    email=models.EmailField()
+    
